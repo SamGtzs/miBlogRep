e086959b9fc50690359fad0be0444c89755a0c8e
samgtzs <samuelgtz025@gmail.com>
2023-10-31 11:47:15 -0600

    index y home

---
+++ docs/home.php
@@ -0,0 +1,57 @@
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Blog Sam</title>
+    <link rel="icon" href="/miBlogRep/static/img/favicon.png">
+    <link rel="stylesheet" href="/miBlogRep/static/css/style.css">
+  </head>
+  <body>
+    <h1>Mi Blog Sam</h1>
+<p>Soy Samuel Gutierrez Sanchez , amante del contenido multimedia y de los videojuegos, lento para todo tipo de cosas</p>
+<p><img src="/miBlogRep/static/img/misa.jpg" alt="ser Feliz"></p>
+<p><a href="/miBlogRep/acerca">Acerca</a></p>
+<h2>Artículos</h2>
+<h3>Categoría Videojuegos Favoritos</h3>
+<ul>
+<li>
+<p><a href="/miBlogRep/blog/videojuego-1/">Reseña Pokemon X</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/videojuego-2/">Reseña Zelda Breath Of The Wild</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/videojuego-3/">Reseña The Binding Of Isaac</a></p>
+</li>
+</ul>
+<h3>Categoria Series Favoritas</h3>
+<ul>
+<li>
+<p><a href="/miBlogRep/blog/serie-1/">Reseña The Flash</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/serie-2/">Reseña Scissor Seven</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/serie-3/">Reseña TrollHunters</a></p>
+</li>
+</ul>
+<h3>Categoria Peliculas Favoritas</h3>
+<ul>
+<li>
+<p><a href="/miBlogRep/blog/pelicula-1/">Reseña Titanes del Pacifico</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/pelicula-2/">Reseña Gigantes de Acero</a></p>
+</li>
+<li>
+<p><a href="/miBlogRep/blog/pelicula-3/">Reseña Como Entrenar a tu Dragon</a></p>
+</li>
+</ul>
+
+<script src="/miBlogRep/static/js/main.js" type="module"></script>
+
+  </body>
+</html>
